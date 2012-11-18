cdb473f82067464169061393584a11150a1adedf
RTS01 <commit1@routetospace.com>
2012-11-18 13:10:52 +0000

    Creating of checksum generator
    Creation of initial structure

---
+++ checksum_generator/checksum_generator.ino
@@ -0,0 +1,16 @@
+/*
+
+This piece of code is to create a checksum based on the RFC 1145.
+This is to be used to determine the checksums for Ublox 6 configuration codes.
+
+*/
+
+long sting;
+int stinlng;
+long stingarr[];
+void setup()
+{
+  Serial.begin (9600);
+}
+
+
