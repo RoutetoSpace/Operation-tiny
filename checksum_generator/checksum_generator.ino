6552747f81ac80b7578d4a9748d7cc60d6c3605f
RTS01 <commit1@routetospace.com>
2012-11-18 19:35:10 +0000

    Correction of coding error
    println was not declared in scope.  realised that println needs to be
    decalred fully.

--- checksum_generator/checksum_generator.ino
+++ checksum_generator/checksum_generator.ino
@@ -5,9 +5,12 @@ This is to be used to determine the checksums for Ublox 6 configuration codes.
 
 */
 
+//include string.h so that I can use the strtok_r function
+
 #include <string.h>
 
 //Idea is to have a script that will add each element of a hex string, seperated by commas, together to produce a final figure. 
+//declare variables
 char *sting(){
   char frame[] = {0x06, 0x24, 0x24, 0x00};
   return frame;
@@ -28,7 +31,7 @@ void loop()
        wor;
        wor = strtok_r(NULL, sep, &brkb))
      { 
-    printf("%s",wor);
+    Serial.println(wor);
      }
 }
 
