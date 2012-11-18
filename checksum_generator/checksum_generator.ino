19ecd66ae9150510da8ff3aee649e95b2fb94c30
RTS01 <commit1@routetospace.com>
2012-11-18 18:26:06 +0000

    Addition of code comments

--- checksum_generator/checksum_generator.ino
+++ checksum_generator/checksum_generator.ino
@@ -5,16 +5,16 @@ This is to be used to determine the checksums for Ublox 6 configuration codes.
 
 */
 
+//include string.h so that I can use the strtok_r function
 #include <string.h>
 
 //Idea is to have a script that will add each element of a hex string, seperated by commas, together to produce a final figure. 
+
+//declare the variables
 char sting;
 int stingin;
 char *sep = ", ";
 char *wordd, *phrase, *brkt;
-
-
-
 
 void setup()
 {
@@ -24,6 +24,8 @@ void setup()
 void loop()
 {
 sting = ("0x06, 0x24, 0x24, 0x00, 0xFF, 0xFF, 0x08, 0x03, 0x00, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00, 0x05, 0x00, 0xFA, 0x00, 0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00");
+  
+  //parse the string
   for (word = strtok_r(sting, sep, &brkt);
     printf(word);
 }
