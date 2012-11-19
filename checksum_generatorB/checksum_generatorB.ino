4717c52ba0d2ef393e29787d9870c908ba54a0b7
RTS01 <commit1@routetospace.com>
2012-11-19 21:25:54 +0000

    re-wrote parse script
    Found article http://letsmakerobots.com/node/11699 which helped me (i
    think) setup a parsing function.  Currently returns syntax errors but
    i'm going to bed.

--- checksum_generatorB/checksum_generatorB.ino
+++ checksum_generatorB/checksum_generatorB.ino
@@ -13,8 +13,8 @@ This is to be used to determine the checksums for Ublox 6 configuration codes.
 //declare variables
 //char *sting()
 String frame = "0x06,0x24,0x24,0x00";
-char *inchar = "!"; 
-char input[4];
+int inchar = ","; 
+char *input[4];
 int stingin;
 char *sep = ", ";
 char *wor, *phrase, *brkt, *brkb;
@@ -27,28 +27,21 @@ void setup()
 
 void loop()
 {
-//  char *frame = sting();
   Serial.println(frame);
-  Serial.write(frame);
-  while (inchar !="!"){
-    inchar = Serial.read();  //wait for the start of the messages
-  }
-  if(inchar ==","{
-    while(Serial.available() < 5){ //wait for 5 characters
+  while (inchar !=","){inchar = Serial.read();}  //wait for the start of the messages
+  if(inchar ==",")
+  {
+    while(Serial.available() < 5) //wait for 5 characters
+	{ 
       ;
     }
-    for (int i=0; i < 5; i++){
+    for (int i=0; i < 5; i++)
+	{
       input[i] = Serial.read(); //Read charactors into an array
     }
    
   }
-  Serial.println(input[1])
-  
-//  for (wor = strtok_r(frame, sep, &brkt);
-//       wor;
-//      wor = strtok_r(NULL, sep, &brkb))
-//     { 
-//   Serial.println(wor);
+  Serial.println(input[1]);
 }
 
 
