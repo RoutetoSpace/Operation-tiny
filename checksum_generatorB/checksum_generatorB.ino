094bc42d92919611bfc914603e03d941aa661a70
RTS01 <commit1@routetospace.com>
2012-11-19 21:04:49 +0000

    Started the cheksum generator again
    Trying to learn how to parse a sting of data so i can then perform
    actions on it.

---
+++ checksum_generatorB/checksum_generatorB.ino
@@ -0,0 +1,56 @@
+/*
+
+This piece of code is to create a checksum based on the RFC 1145.
+This is to be used to determine the checksums for Ublox 6 configuration codes.
+
+*/
+
+//include string.h so that I can use the strtok_r function
+
+#include <string.h>
+
+//Idea is to have a script that will add each element of a hex string, seperated by commas, together to produce a final figure. 
+//declare variables
+//char *sting()
+String frame = "0x06,0x24,0x24,0x00";
+char *inchar = "!"; 
+char input[4];
+int stingin;
+char *sep = ", ";
+char *wor, *phrase, *brkt, *brkb;
+int i;
+
+void setup()
+{
+  Serial.begin (9600);
+}
+
+void loop()
+{
+//  char *frame = sting();
+  Serial.println(frame);
+  Serial.write(frame);
+  while (inchar !="!"){
+    inchar = Serial.read();  //wait for the start of the messages
+  }
+  if(inchar ==","{
+    while(Serial.available() < 5){ //wait for 5 characters
+      ;
+    }
+    for (int i=0; i < 5; i++){
+      input[i] = Serial.read(); //Read charactors into an array
+    }
+   
+  }
+  Serial.println(input[1])
+  
+//  for (wor = strtok_r(frame, sep, &brkt);
+//       wor;
+//      wor = strtok_r(NULL, sep, &brkb))
+//     { 
+//   Serial.println(wor);
+}
+
+
+
+
