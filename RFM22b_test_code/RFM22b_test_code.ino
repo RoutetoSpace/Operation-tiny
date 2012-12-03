95759b8b708e8548079f4a8762e2edbf38319c76
RTS01 <commit1@routetospace.com>
2012-12-03 18:34:42 +0000

    Upload test code
    Test code from http://ukhas.org.uk/guides:rfm22b#tx_ant_and_rx_ant

---
+++ RFM22b_test_code/RFM22b_test_code.ino
@@ -0,0 +1,30 @@
+#include <SPI.h>
+#include <RFM22.h>
+ 
+//Setup radio on SPI with NSEL on pin 10
+rfm22 radio1(10);
+ 
+void setupRadio(){
+ 
+  digitalWrite(5, LOW);
+ 
+  delay(1000);
+ 
+  rfm22::initSPI();
+ 
+  radio1.init();
+ 
+  radio1.write(0x71, 0x00); // unmodulated carrier
+ 
+  //This sets up the GPIOs to automatically switch the antenna depending on Tx or Rx state, only needs to be done at start up
+  radio1.write(0x0b,0x12);
+  radio1.write(0x0c,0x15);
+ 
+  radio1.setFrequency(434.201);
+ 
+  //Quick test
+  radio1.write(0x07, 0x08); // turn tx on
+  delay(1000);
+  radio1.write(0x07, 0x01); // turn tx off
+ 
+}
