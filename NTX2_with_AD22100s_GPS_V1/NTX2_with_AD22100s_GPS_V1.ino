36a0d6b5eba6fb4a118f50e1c48ee94fe94ed639
RTS01 <commit1@routetospace.com>
2012-12-31 12:33:22 +0000

    Adding GPS code to radio transmitter and tmp
    Adding the GPS code to the work with the termometer and Radio

---
+++ NTX2_with_AD22100s_GPS_V1/NTX2_with_AD22100s_GPS_V1.ino
@@ -0,0 +1,226 @@
+/*  NTX2 Radio with temp sensor.  Adapted from NTX2 Radio Test Part 2 by Rob Harrison and M0UPU http://ukhas.org.uk/guides:linkingarduinotontx2
+    GPS code adapted from 2E0UPU and James Coxon http://ukhas.org.uk/guides:ublox6
+    Transmits temp via RTTY with a checksum.
+ 
+    By C Atherton 2012 - www.routetospace.com
+*/ 
+ 
+#define RADIOPIN 13
+#define AD22100S 0 //define temp sensor pin
+#include <string.h>
+#include <util/crc16.h>
+#include <SoftwareSerial.h>
+SoftwareSerial GPS(4, 5);  //RX pin 4, TX pin5
+byte gps_set_sucess = 0 ;
+char datastring[80];
+int Valout;
+unsigned int ADCValue; //ADC = analog digital converter
+ 
+void setup() {                
+  pinMode(RADIOPIN,OUTPUT);
+  ADCValue = analogRead(AD22100S);
+  GPS.begin(9600);
+  // START OUR SERIAL DEBUG PORT
+  Serial.begin(9600);
+  Serial.println("GPS Level Convertor Board Test Script");
+  Serial.println("03/06/2012 2E0UPU");
+  Serial.println("Initialising....");
+  //
+  // THE FOLLOWING COMMAND SWITCHES MODULE TO 4800 BAUD
+  // THEN SWITCHES THE SOFTWARE SERIAL TO 4,800 BAUD
+  //
+  GPS.print("$PUBX,41,1,0007,0003,4800,0*13\r\n"); 
+  GPS.begin(4800);
+  GPS.flush();
+ 
+  //  THIS COMMAND SETS FLIGHT MODE AND CONFIRMS IT 
+  Serial.println("Setting uBlox nav mode: ");
+  uint8_t setNav[] = {
+    0xB5, 0x62, 0x06, 0x24, 0x24, 0x00, 0xFF, 0xFF, 0x08, 0x03, 0x00, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00, 0x05, 0x00, 0xFA, 0x00, 0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xDE, 0xDB                   };
+  while(!gps_set_sucess)
+  {
+    sendUBX(setNav, sizeof(setNav)/sizeof(uint8_t));
+    gps_set_sucess=getUBX_ACK(setNav);
+  }
+  gps_set_sucess=0;
+}
+ 
+void loop() {
+  
+  while(1)
+  {
+    if(GPS.available())
+    {
+      // THIS IS THE MAIN LOOP JUST READS IN FROM THE GPS SERIAL AND ECHOS OUT TO THE ARDUINO SERIAL.
+      Serial.write(GPS.read()); 
+    }
+ 
+  }
+  
+  Valout = map(ADCValue, 0, 1024, -50, 150);
+  sprintf(datastring,"RTTY TEST BEACON, the temperature is %d DEG C", Valout); // Puts the text in the datastring
+  unsigned int CHECKSUM = gps_CRC16_checksum(datastring);  // Calculates the checksum for this datastring
+  char checksum_str[6];
+  sprintf(checksum_str, "*%04X\n", CHECKSUM);
+  strcat(datastring,checksum_str);
+ 
+  rtty_txstring (datastring);
+  delay(2000);
+}
+ 
+// Send a byte array of UBX protocol to the GPS
+void sendUBX(uint8_t *MSG, uint8_t len) {
+  for(int i=0; i<len; i++) {
+    GPS.write(MSG[i]);
+    Serial.print(MSG[i], HEX);
+  }
+  GPS.println();
+}
+// Calculate expected UBX ACK packet and parse UBX response from GPS
+boolean getUBX_ACK(uint8_t *MSG) {
+  uint8_t b;
+  uint8_t ackByteID = 0;
+  uint8_t ackPacket[10];
+  unsigned long startTime = millis();
+  Serial.print(" * Reading ACK response: ");
+ 
+  // Construct the expected ACK packet    
+  ackPacket[0] = 0xB5;	// header
+  ackPacket[1] = 0x62;	// header
+  ackPacket[2] = 0x05;	// class
+  ackPacket[3] = 0x01;	// id
+  ackPacket[4] = 0x02;	// length
+  ackPacket[5] = 0x00;
+  ackPacket[6] = MSG[2];	// ACK class
+  ackPacket[7] = MSG[3];	// ACK id
+  ackPacket[8] = 0;		// CK_A
+  ackPacket[9] = 0;		// CK_B
+ 
+  // Calculate the checksums
+  for (uint8_t i=2; i<8; i++) {
+    ackPacket[8] = ackPacket[8] + ackPacket[i];
+    ackPacket[9] = ackPacket[9] + ackPacket[8];
+  }
+ 
+  while (1) {
+ 
+    // Test for success
+    if (ackByteID > 9) {
+      // All packets in order!
+      Serial.println(" (SUCCESS!)");
+      return true;
+    }
+ 
+    // Timeout if no valid response in 3 seconds
+    if (millis() - startTime > 3000) { 
+      Serial.println(" (FAILED!)");
+      return false;
+    }
+ 
+    // Make sure data is available to read
+    if (GPS.available()) {
+      b = GPS.read();
+ 
+      // Check that bytes arrive in sequence as per expected ACK packet
+      if (b == ackPacket[ackByteID]) { 
+        ackByteID++;
+        Serial.print(b, HEX);
+      } 
+      else {
+        ackByteID = 0;	// Reset and look again, invalid order
+      }
+ 
+    }
+  }
+}
+
+void rtty_txstring (char * string)
+{
+ 
+  /* Simple function to sent a char at a time to 
+   	** rtty_txbyte function. 
+   	** NB Each char is one byte (8 Bits)
+   	*/
+ 
+  char c;
+ 
+  c = *string++;
+ 
+  while ( c != '\0')
+  {
+    rtty_txbyte (c);
+    c = *string++;
+  }
+}
+ 
+ 
+void rtty_txbyte (char c)
+{
+  /* Simple function to sent each bit of a char to 
+   	** rtty_txbit function. 
+   	** NB The bits are sent Least Significant Bit first
+   	**
+   	** All chars should be preceded with a 0 and 
+   	** proceded with a 1. 0 = Start bit; 1 = Stop bit
+   	**
+   	*/
+ 
+  int i;
+ 
+  rtty_txbit (0); // Start bit
+ 
+  // Send bits for for char LSB first	
+ 
+  for (i=0;i<7;i++) // Change this here 7 or 8 for ASCII-7 / ASCII-8
+  {
+    if (c & 1) rtty_txbit(1); 
+ 
+    else rtty_txbit(0);	
+ 
+    c = c >> 1;
+ 
+  }
+ 
+  rtty_txbit (1); // Stop bit
+  rtty_txbit (1); // Stop bit
+}
+ 
+void rtty_txbit (int bit)
+{
+  if (bit)
+  {
+    // high
+    digitalWrite(RADIOPIN, HIGH);
+  }
+  else
+  {
+    // low
+    digitalWrite(RADIOPIN, LOW);
+ 
+  }
+ 
+  //                  delayMicroseconds(3370); // 300 baud
+  delayMicroseconds(10000); // For 50 Baud uncomment this and the line below. 
+  delayMicroseconds(10150); // You can't do 20150 it just doesn't work as the
+                            // largest value that will produce an accurate delay is 16383
+                            // See : http://arduino.cc/en/Reference/DelayMicroseconds
+ 
+}
+ 
+uint16_t gps_CRC16_checksum (char *string)
+{
+  size_t i;
+  uint16_t crc;
+  uint8_t c;
+ 
+  crc = 0xFFFF;
+ 
+  // Calculate checksum ignoring the first two $s
+  for (i = 2; i < strlen(string); i++)
+  {
+    c = string[i];
+    crc = _crc_xmodem_update (crc, c);
+  }
+ 
+  return crc;
+}    
