993baeb7fa5843dabf4cd5df939689a5ab01e926
Chris Atherton <chris.atherton@routetospace.com>
2013-04-21 21:23:21 +0100

    Simple sketch to test function idea

---
+++ test_sketch/test_sketch.ino
@@ -0,0 +1,25 @@
+#include <string.h>
+
+void setup() {
+  // initialize serial:
+  delay(2500);
+  Serial.begin(9600);
+  // reserve 200 bytes for the inputString:
+  
+}
+
+void loop() {
+  
+  lineprt;
+delay(2500);  
+  }
+  
+void lineprt(){
+  Serial.println("RTSHAB01 ");
+  Serial.println("High alititude tracker for use in habbing, developed by Chris Atherton ");
+  Serial.println("NTX2 code from UPU ");
+  Serial.println("TinyGPS code from Mikal Hart ");
+  Serial.println("Checksum code from Lunar_Lander ");
+  Serial.println("http://www.RoutetoSpace.com ");
+  Serial.println();
+}
